a9d72615c6660a3971301be724da552c93b27948
iminiaki <iminiaki@gmail.com>
2025-10-05 23:14:25 +0330

    Remove all generated Next.js build artifacts and cache files to clean up the project structure. This includes manifest files, cached data, and static assets, ensuring a fresh start for the Next.js application.

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> ../utils/CursorFollower\":{\"id\":\"app/page.tsx -> ../utils/CursorFollower\",\"files\":[\"static/chunks/_app-pages-browser_src_utils_CursorFollower_tsx.js\"]}}"
